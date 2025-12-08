8f87181a28c0ac1f234c86f78462a23f1371b026
admin <admin@example.com>
2025-12-08 12:31:36 +0000

    Remove all VirtualSpaceClient files

--- Source/VirtualSpaceClient/GameInstance/VSGameMode.cpp
+++
@@ -1,8 +0,0 @@
-#include "VSGameMode.h"
-#include "UI/VSMainHUD.h"
-
-AVSGameMode::AVSGameMode()
-{
-	// 设置默认 HUD 类
-	HUDClass = AVSMainHUD::StaticClass();
-}
--- Source/VirtualSpaceClient/VirtualSpaceClient.cpp
+++
@@ -1,6 +0,0 @@
-#include "VirtualSpaceClient.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, VirtualSpaceClient, "VirtualSpaceClient");
-
-DEFINE_LOG_CATEGORY(LogVirtualSpace);
